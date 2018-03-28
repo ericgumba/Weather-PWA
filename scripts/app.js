c4a3df469712cb881d98b867d80db839ec064db9
okmanl <ericgumba@gmail.com>
2018-03-27 22:30:33 -0700

    removed things

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,3 @@
-// Copyright 2016 Google Inc.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//      http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-
 (function() {
     'use strict';
 
@@ -326,17 +311,6 @@
     // TODO uncomment line below to test app with fake data
     // app.updateForecastCard(initialWeatherForecast);
 
-    // TODO add startup code here
-    /************************************************************************
-     *
-     * Code required to start the app
-     *
-     * NOTE: To simplify this codelab, we've used localStorage.
-     *   localStorage is a synchronous API and has serious performance
-     *   implications. It should not be used in production applications!
-     *   Instead, check out IDB (https://www.npmjs.com/package/idb) or
-     *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
-     ************************************************************************/
 
     app.selectedCities = localStorage.selectedCities
     if (app.selectedCities) {
